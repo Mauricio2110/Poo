4ed8dfb287a25ff3786691e024e32794ce13e990
Mauricio2110 <54376222+Mauricio2110@users.noreply.github.com>
2019-09-19 18:28:38 -0700

    Update Pelicula.cs

--- Pelicula.cs
+++ Pelicula.cs
@@ -2,15 +2,67 @@ using System;
 
 namespace Pelicula
 {
-
     class Pelicula
     {
-        public String Titulo;
-        public Int16 Año;
-        public String Pais;
-        public String Director;
+        private String Titulo;
+        private Int16 Año;
+        private String Pais;
+        private String Director;
+
+        public void SetTitulo(string titulo)
+        {
+            this.Titulo = titulo;
+        }
+
+        public String GetTitulo()
+        {
+            return this.Titulo;
+        }
+
+        public void SetAño(Int16 año)
+        {
+            this.Año = año;
+        }
+
+        public Int16 GetAño()
+        {
+            return this.Año;
+        }
+
+        public void SetDirector(string director)
+        {
+            this.Director = director;
+        }
+
+        public String GetDirector()
+        {
+            return this.Director;
+        }
+
+        public void SetPais(string pais)
+        {
+            this.Pais = pais;
+        }
+
+        public String GetPais()
+        {
+            return this.Pais;
+        }
+
+        public Pelicula()
+        {
+
+        }
+        public Pelicula(string titulo, Int16 año, string director, string pais)
+        {
+            this.Titulo = titulo;
+            this.Año = año;
+            this.Director = director;
+            this.Pais = pais;
+        }
 
     }
+
     class Program
     {
         static void Main(string[] args)
@@ -18,18 +70,19 @@ namespace Pelicula
             Pelicula p1 = new Pelicula();
             Pelicula p2 = new Pelicula();
 
-            p1.Titulo = "Spiderman";
-            p1.Año = 2018;
-            p1.Pais = "Estados Unidos";
-            p1.Director = "Peter Ramsey";
+            p1.SetTitulo("Spiderman:into the spiderverse");
+            p1.SetAño(2018);
+            p1.SetDirector("Peter Ramsey");
+            p1.SetPais("Estados Unidos");
 
-            p2.Titulo = "Avatar";
-            p2.Año = 2009;
-            p2.Pais = "Estados Unidos";
-            p2.Director = "James Cameron";
+            p2.SetTitulo("Avatar");
+            p2.SetAño(2009);
+            p2.SetDirector("James Cameron");
+            p2.SetPais("Estados Unidos");
 
-            Console.WriteLine("{0}, {1}, {2}, {3}\n", p1.Titulo, p1.Año, p1.Pais, p1.Director);
-            Console.WriteLine("{0}, {1}, {2}, {3}\n", p2.Titulo, p2.Año, p2.Pais, p2.Director);
+            Console.WriteLine("{0}, {1}, {2}, {3}\n", p1.GetTitulo(), p1.GetAño(), p1.GetDirector(), p1.GetPais());
+
+            Console.WriteLine("{0}, {1}, {2}, {3}\n", p2.GetTitulo(), p2.GetAño(), p2.GetDirector(), p2.GetPais());
 
             Console.ReadLine();
 
