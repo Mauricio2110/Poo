4e10506e061c82b7303722b367d1fc896b426661
Mauricio2110 <54376222+Mauricio2110@users.noreply.github.com>
2019-10-23 21:31:42 -0700

    Update Duracion.cs

--- Duracion.cs
+++ Duracion.cs
@@ -8,11 +8,11 @@ namespace duracion
         public int minutos;
         public int segundos;
 
-        public Duracion(int horas,int minutos,int segundos)
+        public Duracion(int horas, int minutos, int segundos)
         {
-            this.horas=horas;
-            this.minutos=minutos;
-            this.segundos=segundos;
+            this.horas = horas;
+            this.minutos = minutos;
+            this.segundos = segundos;
 
         }
 
@@ -20,32 +20,51 @@ namespace duracion
         {
             conversion_inversa(segundos);
 
-            
+
 
 
         }
         public void conversion_inversa(int segundos)
         {
-            minutos=(segundos/60);
-            horas=(minutos/60);
+            minutos = (segundos / 60);
+            horas = (minutos / 60);
 
-            this.segundos=segundos-(minutos*60);
-            minutos=minutos-(horas*60);
+            this.segundos = segundos - (minutos * 60);
+            minutos = minutos - (horas * 60);
 
         }
 
         public void print()
         {
-            Console.WriteLine("Horas:"+horas+" Minutos:"+minutos+" Segundos:"+segundos);
+            Console.WriteLine("Horas:" + horas + " Minutos:" + minutos + " Segundos:" + segundos);
         }
         public void conversion()
-            {
+        {
 
-            minutos=(horas*60)+minutos;
-            segundos=(minutos*60)+segundos;
+            minutos = (horas * 60) + minutos;
+            segundos = (minutos * 60) + segundos;
 
-            Console.WriteLine("Los segundos son:"+segundos);
-            }
+            Console.WriteLine("Los segundos son:" + segundos);
+        }
+
+        public static int operator +(Duracion a, Duracion b)
+        {
+            a.segundos = a.segundos + b.segundos;
+            int s = a.segundos;
+
+            a.minutos = (a.segundos / 60);
+            a.horas = (a.minutos / 60);
+
+            a.segundos = a.segundos - (a.minutos * 60);
+            a.minutos = a.minutos - (a.horas * 60);
+
+            Console.WriteLine("Horas:" + a.horas + " Minutos:" + a.minutos + " Segundos:" + a.segundos);
+            Console.Write("Los segundos son:");
+
+            return s;
+ 
+            
+        }
 
     }
 
@@ -54,24 +73,21 @@ namespace duracion
     {
         static void Main(string[] args)
         {
-            Duracion a = new Duracion(2,15,12);
-            Duracion b = new Duracion(0,02,15);
-            Duracion c = new Duracion(2,00,10);
+            Duracion a = new Duracion(2, 15, 12);
+            Duracion b = new Duracion(3, 50, 57);
             Duracion d = new Duracion(8023);
 
-            
+            Console.WriteLine("A");
             a.print();
             a.conversion();
+            Console.WriteLine("\nB");
             b.print();
             b.conversion();
-            c.print();
-            c.conversion();
-
+            Console.WriteLine("\nSuma A+B");
+            Console.WriteLine(a + b);
+            Console.WriteLine("\nD");
             d.print();
-
-
-
-
+            
         }
     }
 }
